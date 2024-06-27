1e3e9111ab19234aece96e3a5d54c1996d8f4859
Sneha Babuji <snehabsivakripa@gmail.com>
2024-06-27 11:56:40 -0400

    Largest Sum Subarray

---
+++ src/Arrays/LargestSumSubarray.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+//Given an integer array nums, find the subarray with the largest sum, and return its sum.
+public class LargestSumSubarray {
+    public int maxSubArray(int[] nums) {
+
+        int max_sum=0;
+
+        return max_sum;
+    }
+    public static void main(String args[]) {
+        LargestSumSubarray s = new LargestSumSubarray();
+        int num[] = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.print(s.maxSubArray(num));
+    }
+}
